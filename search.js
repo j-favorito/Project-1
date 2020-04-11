ef90de8200037d67e9d2e3899572c9db67c4aa7b
Max Hutchison <mhutchi2517@gmail.com>
2020-04-11 09:47:18 -0700

    js file

--- search.js
+++ search.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
-    let cityName = $().val().trim();
-    let stateName = $().val().trim();
-    let brewName = $().val().trim();
-    let zipCode = $().val().trim();
+    // let stateName = $().val().trim();
+    // let brewName = $().val().trim();
+    // let zipCode = $().val().trim();
     
-    
+    function renderSearchInfo (brewData) {
+        console.log(brewData);
+        console.log(brewData[0].name);
+        for (let i = 0; i < brewData.length; i++) {
+            let brewery = brewData[i];
+            breweryCount = i + 1; 
+            let brewList = $("<ul>");
+            
 
-    $().on("click", function(){
-        let queryURL = "https://api.openbrewerydb.org/breweries";
-        let city = "?by_city="
+        };
+    };
+        
+    
+    
+    
+    $("#submit").on("click", function(e){
+        e.preventDefault();
+        
+        let cityName = $("#city").val().trim();
+        console.log(cityName)
+        let city = "?by_city=" + cityName;
+        let queryURL = "https://api.openbrewerydb.org/breweries" + city;
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .then(function(response){
-            console.log(response);
-        });
+        .then(renderSearchInfo);
 
-    })
+    });
+
 
     
 
